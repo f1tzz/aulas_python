fe127dbe2ec69486546757cb2b23fa28c9b524f2
f1tzz <guifeto1@gmail.com>
2025-03-05 19:45:52 -0300

    primeiro teste real de pograma enviado

---
+++ cauculadora-nova.py
@@ -0,0 +1,10 @@
+def cauculadora(x, y ):
+numero_1 = int(input("digite um numero: "))
+numero_2 = int(input("digite um segundo numero: "))
+x = numero_1
+y = numero_2
+resultado = None 
+operação_matematica = input('')
+
+
+return  resultado
